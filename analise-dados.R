fde09d15152e53e08e8522187da96c91d645839f
Pedro Nobrega <pedrognobrega1997@gmail.com>
2026-04-01 10:55:46 -0300

    Analise descritiva dos dados

--- analise-dados.R
+++ analise-dados.R
@@ -1,5 +1,5 @@
 # Script referente a ativade 3 pratica 1, vinculado ao projeto imoveis
-
+library(ggplot2)
 # Leitura do banco de dados
 dados = read.csv("imoveis.csv", header=T, sep=";")
 attach(dados)
@@ -9,10 +9,24 @@ head(dados)
 
 str(dados)
 
-summary(dados)
+
 
 nrow(dados)
 
 #checando se existe NAN
 
 dados |> na.omit() |> nrow()#sem valores NAN
+
+#Analise descritiva
+summary(dados)
+sd(preco)
+sd(metragem)
+sd(idade)
+sd(itens_embutidos)
+sd(imposto_anual)
+
+dados |> ggplot(aes(x = preco)) + geom_histogram(bins = 10, fill = "#69b3a2", color = "white") +
+  labs(title = "Histograma dos precos", x = "Preço", y = "Freq.") + theme_minimal()
+
+dados |> ggplot(aes(x = metragem, y = preco)) + geom_point(color = "red") +
+  labs(title = "Preço x Metragem", x = "Preço", y = "Megtragem") + theme_minimal()
