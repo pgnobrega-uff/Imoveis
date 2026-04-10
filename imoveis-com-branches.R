e22ed3890c96ddf27fcaa59bb426774c3282cd57
Pedro Nobrega <pedrognobrega1997@gmail.com>
2026-04-10 09:43:17 -0300

    Script Esqueleto

---
+++ imoveis-com-branches.R
@@ -0,0 +1,13 @@
+# Script da aula 7 - 06/04 para praticar uso de branches no Git e GitHub(tarefa 2)
+library(ggplot2)
+# 1. Leitura do banco de dados
+dados = read.csv("imoveis.csv", header=TRUE, sep = ";")
+attach(dados)
+# ver estrutura
+str(dados)
+head(dados)
+
+# 2. Gráficos
+
+
+# 3. Medidas descritivas
